8e936a9f11a45c48d99f66ea938221351264f1ae
Drew Harper <druharper@gmail.com>
2020-12-14 17:50:15 -0600

    job details page

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,21 +1,16 @@
 <template>
   <h1>Job Details</h1>
-  <div v-for="job in jobs" :key="job.id">
-    <h2>{{ job.title }}</h2>
-  </div>
+  <p>This job id is {{ id }}</p>
 </template>
 
 <script>
   export default {
-    data() {
-      return {
-        jobs: [
-          { title: 'Ninja UX Designer', id: 1, details: 'lorem 01' },
-          { title: 'Ninja UI Engineer', id: 2, details: 'lorem 02' },
-          { title: 'Ninja Front-End Dev', id: 3, details: 'lorem 03' },
-        ]
-      }
-    }
+    props: ['id'],
+    // data() {
+    //   return {
+    //     id: this.$route.params.id
+    //   }
+    // }
   }
 </script>
 
