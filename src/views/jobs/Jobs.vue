ea3be34d7e49c4ce2bcae2b5a262cd3ff70515dd
Drew Harper <druharper@gmail.com>
2020-12-15 16:30:58 -0600

    json served data

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,22 +1,27 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
-      <h4>{{ job.title }}</h4>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+        <h4>{{ job.title }}</h4>
+      </router-link>
+    </div>
   </div>
+  <div v-else><p>Loading jobs...</p></div>
 </template>
 
 <script>
   export default {
     data() {
       return {
-        jobs: [
-          { title: 'Ninja UX Designer', id: 1, details: 'lorem 01' },
-          { title: 'Ninja UI Engineer', id: 2, details: 'lorem 02' },
-          { title: 'Ninja Front-End Dev', id: 3, details: 'lorem 03' },
-        ]
+        jobs: []
       }
+    },
+    mounted() {
+      fetch('http://localhost:3000/jobs')
+        .then((res) => res.json())
+        .then((data) => this.jobs = data)
+        .catch((err) => console.log(err.message))
     }
   }
 </script>
