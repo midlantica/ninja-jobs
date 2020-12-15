d8c881437bcb053c09c9f3ed85d5212182cee0cd
Drew Harper <druharper@gmail.com>
2020-12-15 09:32:50 -0600

    routing

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,31 @@
     <router-link to="/about">About</router-link> |
     <router-link to="/jobs">Jobs</router-link>
   </div>
+
+  <div class="historyButtons">
+    <button @click="Redirect">Redirect</button>
+    <button @click="back">Go back</button>
+    <button @click="forward">Go forward</button>
+  </div>
+
   <router-view/>
 </template>
+
+<script>
+  export default {
+    methods: {
+      Redirect() {
+        this.$router.push({ name: 'Home' })
+      },
+      back() {
+        this.$router.go(-1)
+      },
+      forward() {
+        this.$router.go(1)
+      }
+    }
+  }
+</script>
 
 <style>
   #app {
@@ -28,4 +51,13 @@
   #nav a.router-link-exact-active {
     color: #42b983;
   }
-  </style>
+
+  .historyButtons button {
+    font-size: .75rem;
+    margin: 0 10px;
+    padding: 10px 14px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>404</h1>
+  <h2>Page not found</h2>
+</template>
+
+<style scoped>
+  h1 {
+    font-size: 12rem;
+    line-height: 1;
+    margin: 0;
+  }
+</style>
